4ed9b3ec1dcac3a910203b660038023ecda7f462
MayanthaS <sapumalmayantha@gmail.com>
2025-10-11 19:24:15 +0530

    learning the equal operator

--- index.php
+++ index.php
@@ -160,6 +160,25 @@
             echo $x;
             echo '<br>';
             echo ++$x;
+            echo '<br>';
+
+            $x =10;
+            $y =10;
+            if($x === $y){
+                echo "true";
+            }
+            else{
+                echo "false";
+            }
+            echo '<br>';
+             $x =10;
+            $y =10;
+            if($x !== $y){
+                echo "true";
+            }
+            else{
+                echo "false";
+            }
     ?>
 </body>
 </html>
