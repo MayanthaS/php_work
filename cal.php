42cb28d64f29f2afa088a8a02297130beb5de3f0
MayanthaS <sapumalmayantha@gmail.com>
2025-10-12 19:26:33 +0530

    complete the html part of the basic cal

--- cal.php
+++ cal.php
@@ -11,6 +11,17 @@
          <input type="text" name="num1" placeholder="Enter first number"><br><br>
          <label for="">Enter Second Number:</label>
          <input type="text" name="num2" placeholder="Enter second number"><br><br>
+
+         <select name="operator">
+            <option value="">Select</option>
+            <option value="add">Add</option>
+            <option value="sub">Subtract</option>
+            <option value="mul">Multiply</option>
+            <option value="div">Divide</option>
+        </select> <br><br>
+
+        <button type="submit">Calculate</button>
+
     </form>
 
     <?php
